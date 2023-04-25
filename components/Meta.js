f07d954a450a9b3c3519605947f0f98185ed8eee
Мария <maryreznik82@gmail.com>
2023-04-25 21:39:40 +0400

    fix: favicon

--- components/Meta.js
+++ components/Meta.js
@@ -7,33 +7,33 @@ export const Meta = () => {
       <title>Eth sign and send</title>
       <meta name="description" content="Eth sign and send messages" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <link
         rel="icon"
-        href="/icons/favicon-16x16.png"
+        href="/favicon-16x16.png"
         sizes="16x16"
         type="image/png"
       />
-      <link rel="icon" href="/icons/apple-touch-icon.png" type="image/png" />
+      <link rel="icon" href="/apple-touch-icon.png" type="image/png" />
       <link
         rel="icon"
-        href="/icons/android-chrome-192x192.png"
+        href="/android-chrome-192x192.png"
         sizes="192x192"
         type="image/png"
       />
       <link
         rel="icon"
-        href="/icons/android-chrome-512x512"
+        href="/android-chrome-512x512"
         sizes="512x512"
         type="image/png"
       />
       <link
         rel="icon"
-        href="/icons/favicon-32x32.png"
+        href="/favicon-32x32.png"
         sizes="32x32"
         type="image/png"
       />
-      <link rel="manifest" href="/icons/site.webmanifest" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };
