a841c444a016e10906e05b423367347111c285ea
Мария <maryreznik82@gmail.com>
2023-04-26 13:59:03 +0400

    fix: wallet

--- components/Input.js
+++ components/Input.js
@@ -2,11 +2,11 @@ import styles from '../styles/input.module.scss'
 import React from 'react';
 import { forwardRef } from 'react';
 
-const Input = forwardRef(function createInput({type, placeholder, required, id, onChange, text}, ref) {
+const Input = forwardRef(function createInput({placeholder, required, id, onChange, text}, ref) {
 
   return (
     <label >
-    <textarea ref={ref} onChange={onChange} className={styles.input} type={type} placeholder={placeholder} required={required} id={id} value={text}/>
+    <textarea ref={ref} onChange={onChange} className={styles.input} placeholder={placeholder} required={required} id={id} value={text}/>
     </label>
   )
 })
