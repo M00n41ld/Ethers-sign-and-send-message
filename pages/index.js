9989179353c52e51efed05a32eb94b431e42bd6e
Мария <maryreznik82@gmail.com>
2023-04-26 13:16:17 +0400

    fix: wallet

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import HomeContent from "@/components/containers/HomeContent";
 import { Connector } from "@/components/Connector";
-import { Web3Button } from "@web3modal/react";
+import { Web3Button, Web3NetworkSwitch } from "@web3modal/react";
 // const chains = [polygonMumbai];
 // const projectId = "2fb4f192745ab54faae004004a0681e5";
 
@@ -27,6 +27,8 @@ export default function Home() {
   return (
     <>
     <Web3Button/>
+
+    <Web3NetworkSwitch />
         {/* <WagmiConfig client={wagmiClient}> */}
         {/* <Connector/>
         <HomeContent />
