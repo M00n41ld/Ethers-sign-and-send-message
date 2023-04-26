10f8d6bfa4d966006d5ef0b2e0abf56523af34a9
Мария <maryreznik82@gmail.com>
2023-04-26 14:12:39 +0400

    fix: wallet

--- components/Connector.js
+++ components/Connector.js
@@ -5,15 +5,19 @@ import { useState, useEffect } from "react";
 
 export function Connector() {
   const { isConnected } = useAccount();
-  const [isLocalConnected, setIsLocalConnected] = useState(false)
+  const [isLocalConnected, setIsLocalConnected] = useState(false);
   useEffect(() => {
-    console.log(isConnected, 'connector')
-   return setIsLocalConnected(isConnected)
-  }, [isConnected])
+    console.log(isConnected, "connector");
+    return setIsLocalConnected(isConnected);
+  }, [isConnected]);
   return (
     <div className={styles.container}>
-        <h2>{isLocalConnected ? 'Successfully connected!' : 'First Connect your wallet'}</h2>
-        <Web3Button />
+      <h2>
+        {isLocalConnected
+          ? "Successfully connected!"
+          : "First Connect your wallet"}
+      </h2>
+      <Web3Button />
     </div>
   );
 }
