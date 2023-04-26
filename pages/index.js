acfa941a595abb21016400f34c12ae1b580a7874
Мария <maryreznik82@gmail.com>
2023-04-26 13:30:51 +0400

    fix: wallet

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,16 @@ import Footer from "@/components/Footer";
 import HomeContent from "@/components/containers/HomeContent";
 import { Connector } from "@/components/Connector";
 import { Web3Button, Web3NetworkSwitch } from "@web3modal/react";
-// const chains = [polygonMumbai];
-// const projectId = "2fb4f192745ab54faae004004a0681e5";
-
-// const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
-// const wagmiClient = createClient({
-//   autoConnect: true,
-//   connectors: w3mConnectors({ projectId, version: 1, chains }),
-//   provider,
-// });
-// const ethereumClient = new EthereumClient(wagmiClient, chains);
-// console.log(ethereumClient)
+import { Meta } from "@/components/Meta";
 
 export default function Home() {
   return (
     <>
-        <Connector/>
-        <HomeContent />
-        <Footer />
+      <Meta />
+      <Header />
+      <Connector />
+      <HomeContent />
+      <Footer />
     </>
   );
 }
